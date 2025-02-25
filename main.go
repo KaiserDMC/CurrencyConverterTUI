45ae15fdb9af0db57b61351147724d429daa751a
Krisztian Fodor <63681999+KaiserDMC@users.noreply.github.com>
2025-02-25 22:43:27 +0200

    core-data-stuff

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
-	"strings"
+	"time"
 
 	"github.com/charmbracelet/huh"
 	"github.com/shopspring/decimal"
 )
 
 type ExchangeRates struct {
-	Base  string             `json:"base_code"`
-	Rates map[string]float64 `json:"rates"`
+	Base               string             `json:"base_code"`
+	Rates              map[string]float64 `json:"rates"`
+	TimeNextUpdateUnix int64              `json:"time_next_update_unix"`
 }
 
 func fetchAndSaveExchangeRates(filename string) error {
@@ -23,39 +25,87 @@ func fetchAndSaveExchangeRates(filename string) error {
 
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatalf("Failed to fetch exchange rates: %v", err)
+		return fmt.Errorf("Failed to fetch exchange rates: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalf("Failed to read GET response: %v", err)
+		return fmt.Errorf("Failed to read GET response: %v", err)
 	}
 
 	err = os.WriteFile(filename, body, 0644)
 	if err != nil {
-		log.Fatalf("Failed to save JSON file: %v", err)
+		return fmt.Errorf("Failed to save JSON file: %v", err)
 	}
 
 	fmt.Println("Exchange rates saved to", filename)
 	return nil
 }
 
+func readExchangeRates(filename string) (*ExchangeRates, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read exchangeRates file: %v", err)
+	}
+
+	var exchangeData ExchangeRates
+	err = json.Unmarshal(data, &exchangeData)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to write json data: %v", err)
+	}
+
+	return &exchangeData, nil
+}
+
 var (
 	originCurrency    string
-	convertedCurrency string
+	convertedCurrency []string
+	inputText         string
 )
 
 func main() {
 	filePath := "exchangeRates.json"
 
-	f := fetchAndSaveExchangeRates(filePath)
-	if f != nil {
-		fmt.Println(f)
+	wd, _ := os.Getwd()
+	if _, err := os.Stat(wd + "/" + filePath); errors.Is(err, os.ErrNotExist) {
+		if err := fetchAndSaveExchangeRates(filePath); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	exchangeData, err := readExchangeRates(filePath)
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
+	// Check if update is needed
+	currentTimeUnix := time.Now().Unix()
+	if currentTimeUnix >= exchangeData.TimeNextUpdateUnix {
+		fmt.Println("Exchange rates are outdated. Updating...")
+		if err := fetchAndSaveExchangeRates(filePath); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		exchangeData, err = readExchangeRates(filePath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	} else {
+		fmt.Println("Exchange rates are up to date. No update needed.")
+	}
+
+	var currencyOptions []huh.Option[string]
+	for currency := range exchangeData.Rates {
+		currencyOptions = append(currencyOptions, huh.NewOption(currency, currency))
+	}
+
 	form := huh.NewForm(
+
 		huh.NewGroup(
 			huh.NewSelect[string]().
 				Title("Choose origin currency:").
@@ -68,33 +118,26 @@ func main() {
 				Value(&originCurrency),
 		),
 		huh.NewGroup(
-			huh.NewSelect[string]().
+			huh.NewMultiSelect[string]().
 				Title("Choose currency to convert to:").
-				Options(
-					huh.NewOption("United States Dollar", "USD"),
-					huh.NewOption("Japanese Yen", "JPY"),
-					huh.NewOption("Bulgarian Lev", "BGN"),
-					huh.NewOption("Euro", "EUR"),
-				).
+				Options(currencyOptions...).
+				Height(25).
+				Filterable(true).
 				Value(&convertedCurrency),
 		),
 	)
 
-	err := form.Run()
-	if err != nil {
+	if err := form.Run(); err != nil {
 		log.Fatal(err)
 	}
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter amount to convert: ")
-	text, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatalf("Error reading input: %v", err)
-	}
+	huh.NewInput().
+		Title("What amount do you wish to convert?").
+		Prompt("?").
+		Value(&inputText).
+		Run()
 
-	text = strings.TrimSpace(text)
-
-	amountToConvert, err := decimal.NewFromString(text)
+	amountToConvert, err := decimal.NewFromString(inputText)
 	if err != nil {
 		log.Fatalf("Error parsing decimal: %v", err)
 	}
